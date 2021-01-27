91f9cca7cb23307d275b4e36d881ddd524a9d26f
Bayer04Leverkusen <49478715+Bayer04Leverkusen@users.noreply.github.com>
2021-01-27 16:20:21 +0800

    Update alu.v

--- alu.v
+++ alu.v
@@ -11,29 +11,29 @@ module alu(A, B, ALUOp, C, Zero);	//alu module
 	integer    i;
        
 	always @( * ) begin
-    case ( ALUOp )
+    		case ( ALUOp )
 			`ALU_NOP:  C = A;                          
-            `ALU_ADD:  C = A + B;                      
-            `ALU_SUB:  C = A - B;                      
-            `ALU_AND:  C = A & B;                      
-            `ALU_OR:   C = A | B;                      
-            `ALU_SLT:  C = (A < B) ? 32'd1 : 32'd0;    
-            `ALU_SLTU: C = ({1'b0, A} < {1'b0, B}) ? 32'd1 : 32'd0;
+            		`ALU_ADD:  C = A + B;                      
+            		`ALU_SUB:  C = A - B;                      
+            		`ALU_AND:  C = A & B;                      
+            		`ALU_OR:   C = A | B;                      
+            		`ALU_SLT:  C = (A < B) ? 32'd1 : 32'd0;    
+            		`ALU_SLTU: C = ({1'b0, A} < {1'b0, B}) ? 32'd1 : 32'd0;
 			`ALU_XOR: C=A ^ B;
 			`ALU_NOR: C=~(A|B);
-            `ALU_SLL: C=B<<A[10:6];
-            `ALU_SRA: C=B>>>A[10:6];
-            `ALU_LUI: C={B[15:0],16'b0};
-            `ALU_SRL: C=B>>A[10:6];
-            `ALU_SLLV: C=B<<A[4:0];
-            `ALU_SRLV: C=B>>A[4:0];
-            `ALU_BGEZ: C=(A>=0)?0:1;
-            `ALU_BLTZ: C=(A<0)?0:1;
-            `ALU_BLEZ: C=(A<=0)?0:1;
-            `ALU_BGTZ: C=(A>0)?0:1;
-            `ALU_SRAV: C=B>>>A[4:0];
-        default:   C = A;                          // Undefined
-    endcase
+            		`ALU_SLL: C=B<<A[10:6];
+            		`ALU_SRA: C=B>>>A[10:6];
+            		`ALU_LUI: C={B[15:0],16'b0};
+            		`ALU_SRL: C=B>>A[10:6];
+            		`ALU_SLLV: C=B<<A[4:0];
+            		`ALU_SRLV: C=B>>A[4:0];
+            		`ALU_BGEZ: C=(A>=0)?0:1;
+            		`ALU_BLTZ: C=(A<0)?0:1;
+            		`ALU_BLEZ: C=(A<=0)?0:1;
+            		`ALU_BGTZ: C=(A>0)?0:1;
+            		`ALU_SRAV: C=B>>>A[4:0];
+        		default:   C = A;                          // Undefined
+    		endcase
 	end // end always
 	
 	assign Zero = (C == 32'b0);
